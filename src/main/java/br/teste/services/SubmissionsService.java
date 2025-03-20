e503ff1400d20a52b0889c62fa290b492f07c543
Pablo <vilelapege1113@gmail.com>
2025-03-20 01:16:18 -0300

    FEAT : Criado o submissionService

--- src/main/java/br/teste/services/SubmissionsService.java
+++ src/main/java/br/teste/services/SubmissionsService.java
@@ -1,8 +1,16 @@
 package br.teste.services;
 
+import br.teste.entity.SubmissionEntity;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 
 @ApplicationScoped
+@Transactional
 public class SubmissionsService {
 
+    //Criar uma nova submissão
+    public SubmissionEntity createSubmission(SubmissionEntity submissionEntity){
+        SubmissionEntity.persist(submissionEntity);
+        return submissionEntity;
+    }
 }
